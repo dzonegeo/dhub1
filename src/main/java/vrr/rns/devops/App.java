ce7d5166025f2af881f0cb1a305d46b31a0b2524
root <root@gmail.com>
2017-07-02 17:16:08 +0530

    first commit with git

---
+++ src/main/java/vrr/rns/devops/App.java
@@ -0,0 +1,13 @@
+package vrr.rns.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
